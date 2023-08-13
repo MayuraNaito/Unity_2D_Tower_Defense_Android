bb1645a207bd5465bc4cad04d0221f0b4ad448b7
mayura <mayura19941122@gmail.com>
2023-08-14 01:10:54 +0900

    拠点体力機能を実装。

--- Assets/Scripts/System/LevelManager.cs
+++ Assets/Scripts/System/LevelManager.cs
@@ -7,6 +7,10 @@ public class LevelManager : MonoBehaviour
 {
     // 現在のウェーブ
     [NonSerialized] public int currentWave;
+    // 実際に値を変更させる体力用変数
+    [NonSerialized] public int currentLife;
+    // 体力の初期設定だけに使う数値
+    [SerializeField] private int life = 10;
 
 
     // シングルトン
@@ -31,6 +35,8 @@ public class LevelManager : MonoBehaviour
     {
         // 現在のウェーブを設定
         currentWave = 1;
+        // 体力を設定
+        currentLife = life;
     }
 
 
@@ -40,15 +46,36 @@ public class LevelManager : MonoBehaviour
         currentWave++;
     }
 
+    // オブジェクトが表示された時に呼ばれる
     private void OnEnable()
     {
+        // イベントに関数を登録
         Spawner.OnWaveCompleted += WaveCompleted;
+        Enemy.OnReachedGoal += ReduceLifes;
     }
 
+    // オブジェクトが非表示になった時に呼ばれる
     private void OnDisable()
     {
+        // イベントから関数を削除
         Spawner.OnWaveCompleted -= WaveCompleted;
+        Enemy.OnReachedGoal -= ReduceLifes;
 
+    }
+
+    // ライフを減らして体力を確認する関数
+    private void ReduceLifes()
+    {
+        currentLife--;
+
+        // 体力が0以下でないか
+        if (currentLife <= 0)
+        {
+            currentLife = 0;
+
+            // ゲームオーバー処理
+            Debug.Log("ゲームオーバー");
+        }
     }
 
 }
